71b960a025010f272fe3def6f5fcf21782911dc0
Anush <sanushkumarofficial@gmail.com>
2022-07-18 23:08:00 +0530

    Friend MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :friends
+
+  #set homes index as default
+  root 'homes#index'
+
+  get 'homes/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
